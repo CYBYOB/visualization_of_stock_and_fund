dd418159fbad3956ffd649f5a7be79a9fb148640
Mac <mac@JunJie-MacBook-Pro.local>
2021-01-16 21:49:05 +0800

    第一次提交

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div class="app">
+    <div class="a">sss</div>
+    <!-- <router-view/> -->
   </div>
 </template>
 
@@ -11,13 +11,10 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="less" scoped>
+.app {
+  .a {
+    color: red;
+  }
 }
 </style>
